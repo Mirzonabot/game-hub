d0d98e767166db5c9c9ecb27aea0a5dcbf7be771
Mirzonabot <mirzonabot.mirzonabotov99@gmail.com>
2024-10-02 01:17:01 +0500

    15 UX with loading enabled

--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -1,9 +1,11 @@
 import { SimpleGrid, Text } from "@chakra-ui/react";
 import useGames from "../hooks/useGame";
 import GameCard from "./GameCard";
+import GameCardSkeliton from "./GameCardSkeliton";
 
 const GameGrid = () => {
-  const { games, error } = useGames();
+  const { games, error, isLoading } = useGames();
+  const skeletons = [1,2,3,4,5,6];
 
   return (
     <>
@@ -18,6 +20,9 @@ const GameGrid = () => {
         padding={'10px'}
         spacing={10}
       >
+        {isLoading && skeletons.map(skeleton => (
+            <GameCardSkeliton />
+        ) )}
         {games.map((game) => (
           <GameCard key={game.id} game={game} />
           

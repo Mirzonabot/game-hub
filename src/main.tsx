7db63a7e917b476139b405aeffbcabc5e955269f
Mirzonabot <mirzonabot.mirzonabotov99@gmail.com>
2024-10-03 00:59:47 +0500

    31 -  implement searching

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { ChakraProvider, ColorModeScript } from '@chakra-ui/react'
 import App from './App.tsx'
+import './index.css'
 import theme from './theme.ts'
 
 createRoot(document.getElementById('root')!).render(
